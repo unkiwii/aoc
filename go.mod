e8b2763367692223cc43d995fa1688a8e3ce1b3c
Lucas G. Sánchez <unkiwii@gmail.com>
2025-12-11 00:13:27 -0300

    lib: add combinations package and gonum/stat/combin dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/unkiwii/aoc
 
 go 1.24.3
+
+require gonum.org/v1/gonum v0.16.0 // indirect
--- lib/combinations/combinations.go
+++ lib/combinations/combinations.go
@@ -1,6 +1,21 @@
 package combinations
 
+import (
+	"gonum.org/v1/gonum/stat/combin"
+)
+
 func Choose[T any](n int, from []T) [][]T {
-	var result [][]T
+	indices := combin.Combinations(len(from), n)
+	return mapIndices(from, indices)
+}
+
+func mapIndices[T any](from []T, indices [][]int) [][]T {
+	result := make([][]T, len(indices))
+	for i, list := range indices {
+		result[i] = make([]T, len(list))
+		for j, index := range list {
+			result[i][j] = from[index]
+		}
+	}
 	return result
 }
